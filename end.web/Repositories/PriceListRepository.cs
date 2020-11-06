7adc8504b33b0d559eb14b85453712504aea515e
arrrushka <58028979+arrrushka@users.noreply.github.com>
2020-11-06 16:34:18 +0600

    Add files via upload

---
+++ end.web/Repositories/PriceListRepository.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Week8Project.Core.Exceptions.Repositories;
+using Week8Project.Infrastructure.Data;
+
+namespace Week8Project.Infrastructure.Repositories
+{
+    public class PriceListRepository<T> : IPriceListRepository<T> where T : class
+    {
+        private readonly DataContext _dbContext;
+
+        public PriceListRepository(DataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public virtual async Task<T> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> AddAsync(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _dbContext.Entry(entity).State = EntityState.Modified;
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext
+                 .Set<T>()
+                 .ToListAsync();
+        }
+
+
+    }
+}
---
+++ end.web/Repositories/ProductRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Week8Project.Core.Entities;
+using Week8Project.Core.Exceptions.Repositories;
+using Week8Project.Infrastructure.Data;
+
+namespace Week8Project.Infrastructure.Repositories
+{
+    public class ProductRepository : PriceListRepository<Product>, IProductRepository
+    {
+        private readonly DbSet<Product> _products;
+
+        public ProductRepository(DataContext dbContext) : base(dbContext)
+        {
+            _products = dbContext.Products; // or dbContext.Set<Product>()
+        }
+
+        public async Task<Product> GetProductWithMinimumPrice()
+        {
+            var min = await _products.MinAsync(p => p.Price);
+            return await _products.FirstOrDefaultAsync(p => p.Price == min);
+        }
+
+        public async Task<Product> GetProduct() => _products;
+
+    }
+}
